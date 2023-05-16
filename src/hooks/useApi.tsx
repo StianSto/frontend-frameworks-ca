fec22650c94e7ae69cfbf455e6ef218701db4261
Stian Stordal <stordalstian@gmail.com>
2023-05-16 21:55:37 +0200

    added useApi hook

---
+++ src/hooks/useApi.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+
+const useApi = (url: string, options?: FetchOptions) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        setIsLoading(true);
+        setIsError(false);
+        const response = await fetch(url, options);
+        const data = await response.json();
+        setData(data);
+      } catch (error) {
+        setIsError(true);
+        setIsLoading(false);
+      } finally {
+        setIsLoading(false);
+      }
+    })();
+  }, [url, options]);
+
+  return { data, isLoading, isError };
+};
+
+export default useApi;
+
+interface FetchOptions {
+  method?: string;
+  headers?: Record<string, string>;
+  body?: string;
+}
