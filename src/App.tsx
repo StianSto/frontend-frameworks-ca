312ba76376eacd4062a218f58d9487407095a0e9
Stian Stordal <stordalstian@gmail.com>
2023-05-16 14:39:19 +0200

    React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import "./sass/App.scss";
-import Header from "./components/ui/Header";
+
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/ui/Layout";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+import Contact from "./pages/Contact";
+import Cart from "./pages/Cart";
+import NotFound from "./pages/NotFound";
 
 export default function App() {
   return (
     <>
-      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />}></Route>
+          <Route path="product/:id" element={<Product />}></Route>
+          <Route path="cart" element={<Cart />}></Route>
+          <Route path="contact" element={<Contact />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Route>
+      </Routes>
     </>
   );
 }
